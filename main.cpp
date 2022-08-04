55fd51685aca9808492552f5748804c36944f61d
Matthew Okashita <okashita.matthew@gmail.com>
2022-08-04 04:04:20 -0400

    Finish texture chapter
    Code needs a little clean up.

--- main.cpp
+++ main.cpp
@@ -72,9 +72,10 @@ int main() {
 	char buff[PATH_MAX];
 	_getcwd(buff, PATH_MAX);
 	string current_working_dir(buff);
+	current_working_dir = current_working_dir.substr(0, 37);
 	
 	// Create file directory prefixes for shaders.
-	const string PREFIX = current_working_dir.substr(0, 37) + "\\Shaders\\";
+	const string PREFIX = current_working_dir + "\\Shaders\\";
 	
 	// Char[] for vertex shader directory.
 	char VERTEX_SHADER_DIR[PREFIX.size() + strlen("vertexShader.glsl")];
@@ -88,59 +89,32 @@ int main() {
 	Shader ourShader(VERTEX_SHADER_DIR, FRAGMENT_SHADER_DIR);
 #pragma endregion
 
-#pragma region Textures
-
-#pragma endregion
-
 #pragma region User Defined Shapes
+//	float vertices[] = {
+//		// Pos (xyz)       // Col (rgb)
+//		0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top
+//		-0.2f, -0.1f, 0.0f, 0.0f, 1.0f, 0.0f, // left
+//		0.2f, -0.1f, 0.0f, 0.0f, 0.0f, 1.0f, // right
+//		0.0f, -0.4f, 0.0f, 1.0f, 1.0f, 1.0f  // bottom
+//	};
+//
+//	unsigned int indices[] = {  // note that we start from 0!
+//		0, 2, 3,   // front left
+//		0, 1, 3    // front right
+//	};
+	
 	float vertices[] = {
-		// Pos (xyz)       // Col (rgb)
-		0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top
-		-0.2f, -0.1f, 0.0f, 0.0f, 1.0f, 0.0f, // left
-		0.2f, -0.1f, 0.0f, 0.0f, 0.0f, 1.0f, // right
-		0.0f, -0.4f, 0.0f, 1.0f, 1.0f, 1.0f  // bottom
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
 	};
 	
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 2, 3,   // front left
-		0, 1, 3    // front right
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
-	
-	float textureCoordinates[] {
-		0.0f, 0.5f, 0.0f, // top
-		-0.2f, -0.1f, 0.0f, // left
-		0.0f, -0.4f, 0.0f  // bottom
-	};
-#pragma endregion
-
-#pragma region User Defined Textures
-	unsigned int texture;
-	glGenTextures(1, &texture); // Set ID to texture.
-	glBindTexture(GL_TEXTURE_2D, texture);
-	
-	// Set texture wrapping and filtering.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // Use more pixelated filtering to scale down.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Use more blurry filtering to scale up.
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	
-	// Load image data.
-	int width, height, nrChannels;
-	unsigned char *data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
-	
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	} else {
-		cout << " Failed to load texture." << endl;
-	}
-	
-	// Free memory.
-	stbi_image_free(data);
 #pragma endregion
 
 #pragma region VBO, VBA, EBO
@@ -171,20 +145,96 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
 	// Set vertex position attribute pointers.
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 	// Set vertex color attribute pointers.
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 	
 	// Generate EBO
 	// ------------
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
-	
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 #pragma endregion
+
+#pragma region Texture Settings
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+#pragma endregion
+
+#pragma region User Defined Textures
+	unsigned int texture1, texture2;
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); // Tell stb_image.h to flip loaded textures on y-axis.
+	
+	// Texture 1
+	// ---------
+	glGenTextures(1, &texture1); // Set ID to texture.
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	
+	// Set texture wrapping and filtering.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // Use more pixelated filtering to scale down.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Use more blurry filtering to scale up.
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// Load image data.
+	
+	// Char[] for vertex shader directory.
+	
+	char TEXTURE1_DIR[current_working_dir.size() + strlen(R"(Textures\container.jpg)")];
+	strcpy(TEXTURE1_DIR, (current_working_dir + R"(Textures\container.jpg)").c_str());
+	unsigned char *data = stbi_load(TEXTURE1_DIR, &width, &height, &nrChannels, 0);
+	
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		cout << " Failed to load texture 1." << endl;
+	}
+	
+	stbi_image_free(data); // Free memory.
+	
+	// Texture 2
+	// ---------
+	glGenTextures(1, &texture2); // Set ID to texture.
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	
+	// Set texture wrapping and filtering.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // Use more pixelated filtering to scale down.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Use more blurry filtering to scale up.
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// Load image data.
+	char TEXTURE2_DIR[current_working_dir.size() + strlen(R"(Textures\awesomeface.png)")];
+	strcpy(TEXTURE2_DIR, (current_working_dir + R"(Textures\awesomeface.png)").c_str());
+	data = stbi_load(TEXTURE2_DIR, &width, &height, &nrChannels, 0);
+	
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		cout << " Failed to load texture 2." << endl;
+	}
+	
+	stbi_image_free(data); // Free memory.
+#pragma endregion
+	
+	ourShader.use(); // Activate our shader before use.
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	ourShader.setInt("texture2", 1);
 	
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -195,8 +245,12 @@ int main() {
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		ourShader.use();
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
 		glfwSwapBuffers(window); // Swaps color buffer.
@@ -206,6 +260,7 @@ int main() {
 	// Relief buffers.
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	
 	// Terminate GLFW and relieve all allocated GLFW resources.
 	glfwTerminate();
