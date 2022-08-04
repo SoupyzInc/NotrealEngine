0e962d30803802c4cff488418e959f6060ed41e3
Matthew Okashita <okashita.matthew@gmail.com>
2022-08-04 05:30:44 -0400

    Finish various homework items
    - Only reverse the happy face.
    - Visualized different texture wrapping methods by changing texture coordinates to outside [0,1].
    - Able to mix ratio of each texture with arrow keys.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+float mixValue = 0.2f;
+
 // Handles key inputs during program execution.
 void processInput(GLFWwindow *window) {
 	// If ESC, then close window.
@@ -28,6 +30,18 @@ void processInput(GLFWwindow *window) {
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	} else {
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+	
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		if (mixValue < 1.0f) {
+			mixValue += 0.01f;
+		}
+	}
+	
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		if (mixValue > 0.0f) {
+			mixValue -= 0.01f;
+		}
 	}
 }
 
@@ -90,6 +104,7 @@ int main() {
 #pragma endregion
 
 #pragma region User Defined Shapes
+// My Prism Thing
 //	float vertices[] = {
 //		// Pos (xyz)       // Col (rgb)
 //		0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top
@@ -102,7 +117,8 @@ int main() {
 //		0, 2, 3,   // front left
 //		0, 1, 3    // front right
 //	};
-	
+
+// Rectangle of 2 triangles
 	float vertices[] = {
 		// positions          // colors           // texture coords
 		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
@@ -111,13 +127,22 @@ int main() {
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
 	};
 	
+	// Example of nonfilled textured areas.
+//	float vertices[] = {
+//		// positions          // colors           // texture coords
+//		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.5f, 1.5f,   // top right
+//		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.5f, -0.5f,   // bottom right
+//		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   -0.5f, -0.5f,   // bottom left
+//		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   -0.5f, 1.5f    // top left
+//	};
+	
 	unsigned int indices[] = {
 		0, 1, 3, // first triangle
 		1, 2, 3  // second triangle
 	};
 #pragma endregion
 
-#pragma region VBO, VBA, EBO
+#pragma region VBO, VBA, EBO, Textures
 	// Create VBO
 	// ----------
 	// Vertex buffer object (VBO) - stores a large number of vertices in the GPU's memory.
@@ -128,8 +153,6 @@ int main() {
 	// GL_ARRAY_BUFFER target will be used to configure the bound buffer, VBO.
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	
-	// Add User Defined Shapes to VBO
-	// ------------------------------
 	// Copy the triangles vertices into the buffer's memory. glBufferData() is specifically
 	// targeted to copy user-defined data into the buffer.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -158,17 +181,20 @@ int main() {
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-#pragma endregion
-
-#pragma region Texture Settings
+	
+	// Textures
+	// --------
+	// Set vertex texture attribute pointers.
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
+	
+	glEnable(GL_BLEND); // Enable blending.
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Allow transparency to work with PNGs.
 #pragma endregion
 
 #pragma region User Defined Textures
 	unsigned int texture1, texture2;
 	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true); // Tell stb_image.h to flip loaded textures on y-axis.
 	
 	// Texture 1
 	// ---------
@@ -176,8 +202,8 @@ int main() {
 	glBindTexture(GL_TEXTURE_2D, texture1);
 	
 	// Set texture wrapping and filtering.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // Use more pixelated filtering to scale down.
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Use more blurry filtering to scale up.
@@ -186,9 +212,6 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	
 	// Load image data.
-	
-	// Char[] for vertex shader directory.
-	
 	char TEXTURE1_DIR[current_working_dir.size() + strlen(R"(Textures\container.jpg)")];
 	strcpy(TEXTURE1_DIR, (current_working_dir + R"(Textures\container.jpg)").c_str());
 	unsigned char *data = stbi_load(TEXTURE1_DIR, &width, &height, &nrChannels, 0);
@@ -204,6 +227,8 @@ int main() {
 	
 	// Texture 2
 	// ---------
+	stbi_set_flip_vertically_on_load(true); // From here on, tell stb_image.h to flip loaded textures on y-axis (only flip texture 2).
+	
 	glGenTextures(1, &texture2); // Set ID to texture.
 	glBindTexture(GL_TEXTURE_2D, texture2);
 	
@@ -221,9 +246,8 @@ int main() {
 	char TEXTURE2_DIR[current_working_dir.size() + strlen(R"(Textures\awesomeface.png)")];
 	strcpy(TEXTURE2_DIR, (current_working_dir + R"(Textures\awesomeface.png)").c_str());
 	data = stbi_load(TEXTURE2_DIR, &width, &height, &nrChannels, 0);
-	
 	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	} else {
 		cout << " Failed to load texture 2." << endl;
@@ -233,8 +257,11 @@ int main() {
 #pragma endregion
 	
 	ourShader.use(); // Activate our shader before use.
-	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	
+	// Tell OpenGL which texture unit each shader sampler belongs to.
+	ourShader.setInt("texture1", 0); // With shader class
 	ourShader.setInt("texture2", 1);
+	
 	
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -250,6 +277,8 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
+		
+		ourShader.setFloat("mixValue", mixValue);
 		
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
