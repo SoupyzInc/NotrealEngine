996944f37f1f1b68ff9717e4f3f9d3bda1e1ea65
Matthew Okashita <okashita.matthew@gmail.com>
2024-01-14 20:56:57 -0500

    Remove using namespace std

--- shaders/shader.h
+++ shaders/shader.h
@@ -8,7 +8,6 @@
 
 #include "glad/glad.h"
 
-using namespace std;
 
 class Shader {
 public:
@@ -19,19 +18,19 @@ public:
     Shader(const char *vertexPath, const char *fragmentPath) {
         // Get vertex source code from file path.
         // --------------------------------------
-        string vertexCode;
-        string fragmentCode;
-        ifstream vShaderFile;
-        ifstream fShaderFile;
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
 
-        // Ensure that ifstream can throw exceptions.
-        vShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
-        fShaderFile.exceptions(ifstream::failbit | ifstream::badbit);
+        // Ensure that std::ifstream can throw exceptions.
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         try {
             // Open files.
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
-            stringstream vShaderStream, fShaderStream;
+            std::stringstream vShaderStream, fShaderStream;
 
             // Read file's buffer into streams.
             vShaderStream << vShaderFile.rdbuf();
@@ -44,8 +43,8 @@ public:
             // Convert stream into string.
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
-        } catch (ifstream::failure e) {
-            cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << endl;
+        } catch (std::ifstream::failure e) {
+            std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
         }
 
         const char *vShaderCode = vertexCode.c_str();
@@ -66,7 +65,7 @@ public:
         glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
         if (!success) {
             glGetShaderInfoLog(vertex, 512, nullptr, infoLog);
-            cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+            std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
         }
 
         // Fragment shaders
@@ -79,7 +78,7 @@ public:
         glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
         if (!success) {
             glGetShaderInfoLog(fragment, 512, nullptr, infoLog);
-            cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+            std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
         }
 
         // Shader Program
@@ -93,7 +92,7 @@ public:
         glGetProgramiv(ID, GL_LINK_STATUS, &success);
         if (!success) {
             glGetProgramInfoLog(ID, 512, nullptr, infoLog);
-            cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+            std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
         }
 
         // Free shader objects from memory.
@@ -108,15 +107,15 @@ public:
 
     // Utility uniform functions.
     // TODO: Add the rest of the utility functions.
-    void setBool(const string &name, bool value) const {
+    void setBool(const std::string &name, bool value) const {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
     }
 
-    void setInt(const string &name, int value) const {
+    void setInt(const std::string &name, int value) const {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
     }
 
-    void setFloat(const string &name, float value) const {
+    void setFloat(const std::string &name, float value) const {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
 
