f5e99a3fac83f54783739a480235e2681efc7c18
Matthew Okashita <okashita.matthew@gmail.com>
2024-01-19 14:58:55 -0500

    Remove comments

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,7 +1,3 @@
-//
-// Created by okash on 1/16/2024.
-//
-
 #include "actor.h"
 
 actor::actor(glm::vec3 pos) {
--- src/actor.h
+++ src/actor.h
@@ -1,7 +1,3 @@
-//
-// Created by okash on 1/16/2024.
-//
-
 #ifndef NOTREALENGINE_ACTOR_H
 #define NOTREALENGINE_ACTOR_H
 
